2fbb2b2906ca310073857dc500153019a435c799
andy <andy@ignignokt.co.uk>
2011-08-28 22:02:55 +0100

    Final test

--- src/uk/co/ignignokt/markov/Main.java
+++ src/uk/co/ignignokt/markov/Main.java
@@ -9,6 +9,8 @@ public class Main {
 
 		System.out.println(wm.getSentence());
 		System.out.println(wm.getSentence());
+		System.out.println(wm.getSentence());
+		System.out.println(wm.getSentence());
 
 		long endTime = System.currentTimeMillis();
 		
